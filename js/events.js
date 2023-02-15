ab7bd4837fabd3df9f1e5065dfe6f61cafed5482
islamajabul <ajabulmd1@gmail.com>
2023-02-15 22:02:14 +0800

    second class add

--- js/events.js
+++ js/events.js
@@ -1,1 +1,18 @@
-console.log('This is separate js file');
+// Option 01: Directly set on the HTML element
+
+
+// Option 02: add onclick function
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+// option 03-1
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+// option 03-2
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple'
+}
