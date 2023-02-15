75a794d429a3947818f02baa5bc43afd71a7be88
islamajabul <ajabulmd1@gmail.com>
2023-02-15 22:31:25 +0800

    Thirdt lesson done

--- js/events.js
+++ js/events.js
@@ -1,18 +1,35 @@
 // Option 01: Directly set on the HTML element
+// <button onclick="console.log(65)">Another Click</button>
 
-
-// Option 02: add onclick function
+// Option 02: add onclick function on the html element[***]
+// <button onclick="makeRed()">Make Red</button>
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
-// option 03-1
+// Option 03-1
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
-// option 03-2
+// Option 03-2 [****]
 const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple'
 }
+// Option 04-1
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+// Option 04-2
+const makeGreenButton = document.getElementById('make-green')
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+// Option 04-3 [*****]
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenRod';
+}) 
